a0d47686869ed4a6207e706e371fbcf6ad89acf1
hatem <gadallah.hatem@gmail.com>
2025-04-04 08:48:18 +0400

    bla bla

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://malazinvestment.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
